424a92d1393e27b8fc0dbd7ad9faaa93ad01c98e
nemanja992 <nemanja_muncan@yahoo.com>
2018-05-25 02:34:00 +0200

    added routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs')
 const app = express();
 
 app.use(bodyParser.json());
@@ -51,6 +52,35 @@ app.post('/register', (req,res)=> {
 			joined: new Date ()
 	})
 	res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req,res)=> {
+	const {id} = req.params;
+	let found = false;
+	database.users.forEach(users => {
+		if (user.id === id) {
+			found=true;
+			return res.json(user);
+		} 
+	})
+	if(!found) {
+		res.status(400).json('not found')
+	}
+})
+
+app.put('/image', (req,res)=> {
+	const {id} = req.body;
+	let found = false;
+	database.users.forEach(users => {
+		if (user.id === id) {
+			found=true;
+			user.entries++;
+			return res.json(user.entries);
+		} 
+	})
+	if(!found) {
+		res.status(400).json('not found')
+	}
 })
 
 app.listen(3000, ()=> {
